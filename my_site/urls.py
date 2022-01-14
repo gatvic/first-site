c4972c7c2089f5551c8e1bae412202cc8f8dd12f
gatvic <gatvic@yandex.ru>
2022-01-14 22:48:59 +0300

    begin

--- my_site/urls.py
+++ my_site/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from blog.views import index
+from plant_calc.views import index_2
 
 urlpatterns = [
+    path('', include('blog.urls')),
+    path('plant_calc/', include('plant_calc.urls')),
     path('admin/', admin.site.urls),
 ]
