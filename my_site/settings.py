960c9dd32d4e7f1cd4a330cd92d9bf1a14dcd712
gatvic <gatvic@yandex.ru>
2022-01-17 15:14:05 +0300

    change db

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@e-!^n44p9t+(4xb!5g&=2(1do3)&qt=zb1w-cfx*cmeq38a=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gatvic.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
