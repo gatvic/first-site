a1ee4300c052ad1dfd4de01187cdce5a134929f1
gatvic <gatvic@yandex.ru>
2022-01-18 16:25:31 +0300

    change models fin half day

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
-    path('post/<int:pk>', post_detail, name='post_detail'),
+    path('', post_list),
+#    path('post/<int:pk>', post_detail, name='post_detail'),
+]
+
+urlpatterns += [
+    path('/int:cat_id>/', by_cat, name='by_cat'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render
+# , get_list_or_404
 # from django.utils import timezone
-# from django.http import HttpResponse, Http404
-from .models import Post
+# from django.http import HttpResponse
+# , Http404
+from .models import Post, Cat
 
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    cats = Cat.objects.all()
+    context = {'posts': posts, 'cats': cats}
+    return render(request, 'blog/base.html', context)
 
-def post_detail(request, pk):
-    post = get_list_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+
+# def post_detail(request, pk):
+#    post = get_list_or_404(Post, pk=pk)
+#    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def by_cat(request, cat_id):
+    posts = Post.objects.filter(cat=cat_id)
+    cats = Cat.objects.all()
+    current_cat = Cat.objects.get(pk=cat_id)
+    context = {'posts': posts, 'cats': cats, 'current_cats': current_cat}
+    return render(request, 'blog/by_cat.html', context)
