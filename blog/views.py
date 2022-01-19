720f612020bca46c1e331a343d14f14eb52ea39d
gatvic <gatvic@yandex.ru>
2022-01-19 07:46:28 +0300

    add form

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 # , Http404
 from .models import Post, Cat
+from django.urls import reverse_lazy
+
+from django.views.generic.edit import CreateView
+
+from .forms import PostForm
 
 
 def post_list(request):
@@ -24,3 +29,14 @@ def by_cat(request, cat_id):
     current_cat = Cat.objects.get(pk=cat_id)
     context = {'posts': posts, 'cats': cats, 'current_cat': current_cat}
     return render(request, 'blog/by_cats.html', context)
+
+
+class PostCreateView(CreateView):
+    template_name = 'blog/create.html'
+    form_class = PostForm
+    success_url = reverse_lazy('base')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cats'] = Cat.objects.all()
+        return context
