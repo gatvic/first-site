5eac39fd102c71e3c8880fa9aba204aee0d293fe
gatvic <gatvic@yandex.ru>
2022-01-20 09:43:15 +0300

    fix template

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('/<int:cat_id>/', by_cat, name='by_cat'),
-    path('', post_list),
+    path('', post_list, name='post_list'),
     path('add/', PostCreateView.as_view(), name='add'),
 #    path('post/<int:pk>', post_detail, name='post_detail'),
 ]
