d54d7888921062923b22aeb783e51d188459a29f
gatvic <gatvic@yandex.ru>
2022-01-20 11:56:38 +0300

    new templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/<int:cat_id>/', by_cat, name='by_cat'),
+    path('<int:cat_id>/', by_cat, name='by_cat'),
     path('', post_list, name='post_list'),
     path('add/', PostCreateView.as_view(), name='add'),
 #    path('post/<int:pk>', post_detail, name='post_detail'),
--- my_site/urls.py
+++ my_site/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from blog.views import *
-from plant_calc.views import *
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('plant_calc/', include('plant_calc.urls')),
     path('admin/', admin.site.urls),
-#    path('cats/<slug:cat>/', categories),
+
 ]
